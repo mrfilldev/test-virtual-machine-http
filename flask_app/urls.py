12a950babc67d9a0f103063bd013f3a7c8254364
Mr.Fill <mr.filldev@gmail.com>
2023-02-15 19:19:01 +0300

    added __init__.py for visualize

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -3,7 +3,6 @@ import os
 
 import boto3
 from flask import Flask, request, Response
-
 from flask_app import carwash_list, carwash_order
 import ping_carwash_box
 from flask_app.carwash_order import Status
