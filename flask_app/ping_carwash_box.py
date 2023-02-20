4cf92b3a9767365bc2eb671e2938d9725dda1349
Mr.Fill <mr.filldev@gmail.com>
2023-02-20 22:55:50 +0300

    изменение пинг

--- flask_app/ping_carwash_box.py
+++ flask_app/ping_carwash_box.py
@@ -3,29 +3,20 @@ def main(request):
     box_number = request.args.get('boxNumber')
     print('carwash_id: ', carwash_id)
     print('box_number: ', box_number)
-    if carwash_id == '1':
-        return carwash_id_1(box_number)
-    if carwash_id == '2':
-        return carwash_id_2(box_number)
+
+    return carwash_id(box_number)
 
 
-def carwash_id_1(box_number):
+def carwash_id(box_number):
     if box_number == '1':
         status = 200
-        print('status_reply',  status)
+        print('status_reply', status)
         return status
     elif box_number == '2':
-        status = 404
-        print('status_reply',  status)
-        return status
-
-
-def carwash_id_2(box_number):
-    if box_number == '1':
         status = 200
-        print('status_reply',  status)
+        print('status_reply', status)
         return status
-    elif box_number == '2':
+    elif box_number == '3':
         status = 404
-        print('status_reply',  status)
+        print('status_reply', status)
         return status
