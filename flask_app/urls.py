135ee25dcbc604cca72dc0ccae6f0b703bd7f88d
Mr.Fill <mr.filldev@gmail.com>
2023-02-21 20:33:24 +0300

    fix

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -70,19 +70,6 @@ async def make_carwash_order():
         print(f'caught {type(e)}: e')  # добавить логгер
         return Response(status=400)
 
-@app.route('/tanker/order', methods=['POST'])
-async def make_carwash_order():
-    try:
-        carwash_order.update_order(request)
-        return Response(status=200)
-    except Exception as e:
-        # write to log
-        print(f'caught {type(e)}: e')  # добавить логгер
-        return Response(status=400)
-
-
-
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
