2bee11d4ff93011acf20dd88009d2ac5f79938e4
Mr.Fill <mr.filldev@gmail.com>
2023-02-21 16:30:32 +0300

    fix

--- daemon/__init__.py
+++ daemon/__init__.py
@@ -2,4 +2,4 @@ import daemon.start_point
 import daemon.daemon
 
 from daemon.daemon import send_canceled_status, get_order_messege_queue
-from start_point import url, dbs
+from daemon.start_point import url, dbs
