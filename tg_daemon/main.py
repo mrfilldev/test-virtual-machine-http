9781e0ef87dc4c4a04084208b3979ee0a1cd58bb
Mr.Fill <mr.filldev@gmail.com>
2023-02-21 16:31:13 +0300

    fix

--- tg_daemon/main.py
+++ tg_daemon/main.py
@@ -4,24 +4,24 @@ from dotenv import load_dotenv
 import os
 from urllib.parse import quote_plus as quote
 import pymongo
-from daemon.start_point import dbs
+#from daemon.start_point import dbs
 
 load_dotenv()
 
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 bot = Bot(token=os.getenv('BOT_TOKEN'))
-#
-# url = 'mongodb://{user}:{pw}@{hosts}/?replicaSet={rs}&authSource={auth_src}'.format(
-#     user=quote('user1'),
-#     pw=quote('mrfilldev040202'),
-#     hosts=','.join([
-#         'rc1a-f0wss58juko3mx2p.mdb.yandexcloud.net:27018'
-#     ]),
-#     rs='rs01',
-#     auth_src='db1')
-# dbs = pymongo.MongoClient(
-#     url,
-#     tlsCAFile='/home/mrfilldev/.mongodb/root.crt')['db1']
+
+url = 'mongodb://{user}:{pw}@{hosts}/?replicaSet={rs}&authSource={auth_src}'.format(
+    user=quote('user1'),
+    pw=quote('mrfilldev040202'),
+    hosts=','.join([
+        'rc1a-f0wss58juko3mx2p.mdb.yandexcloud.net:27018'
+    ]),
+    rs='rs01',
+    auth_src='db1')
+dbs = pymongo.MongoClient(
+    url,
+    tlsCAFile='/home/mrfilldev/.mongodb/root.crt')['db1']
 
 
 async def get_amount_orders():
