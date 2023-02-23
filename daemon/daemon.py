b36d8eb01979b54155668471c146378c57d0ecae
Mr.Fill <mr.filldev@gmail.com>
2023-02-23 22:58:20 +0300

    fix

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -111,7 +111,8 @@ async def user_canceled(order_json):
         #  проверку в бд
         order_in_db = mycol.find({'Id': str(order_json.Id)})
         print('ORDER_IN_DB: ', type(order_in_db), order_in_db)
-        order = json.loads(order_in_db, object_hook=lambda d: SimpleNamespace(**d))
+        order = json.dumps(order_in_db, object_hook=lambda d: SimpleNamespace(**d))
+        order = json.loads(order, object_hook=lambda d: SimpleNamespace(**d))
 
         print('Status: ', order.Status)
 
