84a414663055f837c093560cfaf448c550e66f88
Mr.Fill <mr.filldev@gmail.com>
2023-02-26 15:31:47 +0300

    fix

--- flask_app/start-service.sh
+++ flask_app/start-service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/mrfilldev/test-server/test-virtual-machine-http/venv/bin/python3 /home/mrfilldev/test-virtual-machine-http/flask_app/urls.py
+/home/mrfilldev/test-server/test-virtual-machine-http/venv/bin/python3 /home/mrfilldev/test-server/test-virtual-machine-http/flask_app/urls.py
