49b21be58615139fd068839edaa5057f26a36017
Mr.Fill <mr.filldev@gmail.com>
2023-02-26 13:58:10 +0300

    work on admin zone

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -76,8 +76,22 @@ async def make_carwash_order():
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'mr Fill'}
-    return render_template('index.html', user=user)
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 if __name__ == '__main__':
