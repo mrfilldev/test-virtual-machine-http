4a367c499ac2c039e11e8633b5def1c4b1c37926
Mr.Fill <mr.filldev@gmail.com>
2023-02-26 16:44:14 +0300

    login form

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -101,14 +101,5 @@ def login():
 
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
-
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
