e454d3e7c3c252ab2c957ac9135d2977ce76833b
Mr.Fill <mr.filldev@gmail.com>
2023-02-28 13:33:40 +0300

    modyfing the index

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -84,20 +84,13 @@ def login():
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Лерк))!'}
+    user = {'username': 'No Name))!'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': user['username']},
+            'body': 'Lets make some noize!'
         },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Ипполит'},
-            'body': 'Какая гадость эта ваша заливная рыба!!'
-        }
+
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
