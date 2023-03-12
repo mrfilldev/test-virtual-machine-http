84305dd613ae7ea96ef3d9ada69a437d0bfb027a
Mr.Fill <mr.filldev@gmail.com>
2023-03-12 14:43:29 +0300

    fix

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, RadioField, FloatField, FieldList
+from wtforms.validators import DataRequired, Email
 
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+class CarwashForm(FlaskForm):
+    name = StringField("Name: ", validators=[DataRequired()])
+    id = StringField("id: ", validators=[DataRequired()])
+    enable = BooleanField("enable: ", validators=[DataRequired()])
+    address = StringField("address: ", validators=[DataRequired()])
+    Location = StringField("Location: ", validators=[DataRequired()])
+    Type = RadioField("Name: ", validators=[DataRequired()])
+    stepCost = FloatField("stepCost: ", validators=[DataRequired()])
+    limitMinCost = FloatField("limitMinCost: ", validators=[DataRequired()])
+    Price = FloatField("Price: ", validators=[DataRequired()])
+    Boxes = StringField("stepCost: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
