43a1c51b6d527f90314bc9554b0783ee2d35df99
Mr.Fill <mr.filldev@gmail.com>
2023-03-12 21:53:16 +0300

    fix

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -5,19 +5,20 @@ from wtforms.validators import DataRequired, Email
 
 class CarwashForm(FlaskForm):
     name = StringField("Название: ", validators=[DataRequired()])
-    # id = StringField("id: ", validators=[DataRequired()])
-    # enable = BooleanField("enable: ", validators=[DataRequired()])
     Address = StringField("Адрес: ", validators=[DataRequired()])
     Location = StringField("Местоположение: ", validators=[DataRequired()])
-    Type = RadioField('Type', choices=[('SelfServiceFixPrice', 'SelfServiceFixPrice'),
-                                       ('SelfService', 'SelfService'),
-                                       ('Manual', 'Manual'),
-                                       ('Portal', 'Portal'),
-                                       ('Tunnel', 'Tunnel'),
-                                       ('Dry', 'Dry')
-                                       ])
-    StepCost = FloatField("stepCost: ", validators=[DataRequired()])
-    LimitMinCost = FloatField("limitMinCost: ", validators=[DataRequired()])
-    Price = FloatField("Цена: ", validators=[DataRequired()])
-    # Boxes = StringField("stepCost: ", validators=[DataRequired()])
     Submit = SubmitField("Создать")
+
+    # Type = RadioField('Type', choices=[('SelfServiceFixPrice', 'SelfServiceFixPrice'),
+    #                                    ('SelfService', 'SelfService'),
+    #                                    ('Manual', 'Manual'),
+    #                                    ('Portal', 'Portal'),
+    #                                    ('Tunnel', 'Tunnel'),
+    #                                    ('Dry', 'Dry')
+    #                                    ])
+    # StepCost = FloatField("stepCost: ", validators=[DataRequired()])
+    # LimitMinCost = FloatField("limitMinCost: ", validators=[DataRequired()])
+    # Price = FloatField("Цена: ", validators=[DataRequired()])
+    # Boxes = StringField("stepCost: ", validators=[DataRequired()])
+    # id = StringField("id: ", validators=[DataRequired()])
+    # enable = BooleanField("enable: ", validators=[DataRequired()])
