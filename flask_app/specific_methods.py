4273b4594be3294817e7a0eaa4859addc109330f
Mr.Fill <mr.filldev@gmail.com>
2023-03-15 03:37:04 +0300

    fix

--- flask_app/specific_methods.py
+++ flask_app/specific_methods.py
@@ -20,52 +20,37 @@ def method_of_filters(request):
     for i in request.form:
         print(i, request.form[i])
 
-    if request.form['search_field'] == '' and request.form['status'] == '':
-        print('0 0')
-
-    if request.form['search_field'] == '' and request.form['status'] != '':
-        print('0 1')
-
-    if request.form['search_field'] != '' and request.form['status'] == '':
-        print('1 0')
-
     if request.form['search_field'] != '' and request.form['status'] != '':
-        print('1 1')
+        print('HERE')
+        if is_valid_guid(request.form['search_field']):
+            print('is_guid')
+            print('search_field & status')
+            result = {
+                'Id': request.form['search_field'],
+                'Status': request.form['status']
+                # 'DateCreate': {'$gt': start_time.isoformat()}
+            }
+        else:
+            print('not_guid')
+            print('search_field & status')
+            result = {
+                'CarWashId': request.form['search_field'],
+                'Status': request.form['status']
+                # 'DateCreate': {'$gt': start_time.isoformat()}
+            }
+    elif request.form['search_field'] != '' and request.form['status'] == '':
+        print('only search_field')
+        result = {
+            'CarWashId': request.form['search_field'],
+            # 'DateCreate': {'$gt': start_time.isoformat()}
+        }
+    elif request.form['search_field'] == '' and request.form['status'] != '':
+        print('only status')
+        result = {
+            'Status': request.form['status']
+            # 'DateCreate': {'$gt': start_time.isoformat()}
+        }
+    print('search_field', request.form['search_field'])
+    print('status', request.form['status'])
 
     return result
-
-#     for i in request.form:
-#         print(i, request.form[i])
-#
-#     if request.form['search_field'] != '' and request.form['status'] != '':
-#         print('HERE')
-#         if is_valid_guid(request.form['search_field']):
-#             print('is_guid')
-#             print('search_field & status')
-#             result = {
-#                 'Id': request.form['search_field'],
-#                 'Status': request.form['status']
-#                 # 'DateCreate': {'$gt': start_time.isoformat()}
-#             }
-#         else:
-#             print('not_guid')
-#             print('search_field & status')
-#             result = {
-#                 'CarWashId': request.form['search_field'],
-#                 'Status': request.form['status']
-#                 # 'DateCreate': {'$gt': start_time.isoformat()}
-#             }
-#     elif request.form['search_field'] != '' and request.form['status'] == '':
-#         print('only search_field')
-#         result = {
-#             'CarWashId': request.form['search_field'],
-#             # 'DateCreate': {'$gt': start_time.isoformat()}
-#         }
-#     elif request.form['search_field'] == '' and request.form['status'] != '':
-#         print('only status')
-#         result = {
-#             'Status': request.form['status']
-#             # 'DateCreate': {'$gt': start_time.isoformat()}
-#         }
-#     print('search_field', request.form['search_field'])
-#     print('status', request.form['status'])
