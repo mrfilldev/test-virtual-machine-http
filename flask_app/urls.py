5949b1d2ee413b32268b51788164395ad78f91a9
Mr.Fill <mr.filldev@gmail.com>
2023-03-16 16:56:19 +0300

    adding yandex oauth

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -325,7 +325,7 @@ def order_detail(order_id):
 def profile():
     #username = dict(session)['username']
     user_inf = oauth_via_yandex.get_user(session['ya-token'])
-
+    print(user_inf)
     return render_template('profile/profile.html', context=user_inf)
 
 
