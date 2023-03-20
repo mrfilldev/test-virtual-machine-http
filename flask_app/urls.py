c1f70344ed7fd1f2c62fd264b227a534b596b033
Mr.Fill <mr.filldev@gmail.com>
2023-03-20 20:08:40 +0300

    admin_zone start

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -442,7 +442,7 @@ def fill_company():
             },
         }
         new_order = Config.col_orders.update_one(user_yan_inf['id'], set_command)
-        return redirect(url_for('profile'))
+        return render_template(url_for('profile'))
     context = {
         'status': status,
         'user': user,
