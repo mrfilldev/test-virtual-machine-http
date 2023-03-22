135ecf04b222fdec310758ca72fa5413b86a78cc
Mr.Fill <mr.filldev@gmail.com>
2023-03-22 18:00:38 +0300

    admin_zone start

--- scripts/script_delete_user.py
+++ scripts/script_delete_user.py
@@ -1,6 +1,6 @@
 from config.config import Config
 
-x = Config.col_users.delete_one({"_id": 'namespace($oid="641aea4acdb05bc1a79167aa")'})
-print(f'user: 641aea4acdb05bc1a79167ab has been deleted ', x)
-x = Config.col_users.delete_one({"_id": 'namespace($oid="641aea4acdb05bc1a79167ab")'})
-print(f'user: 641aea4acdb05bc1a79167ab has been deleted ', x)
+x = Config.col_users.delete_one({"name": 'Admin'})
+print(f'user: Admin has been deleted ', x)
+x = Config.col_users.delete_one({"name": 'Username'})
+print(f'user: Username has been deleted ', x)
