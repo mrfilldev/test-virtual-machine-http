4a73ab4fad82e1406c9d9ada5dafdd68e2421f87
Mr.Fill <mr.filldev@gmail.com>
2023-03-23 19:32:11 +0300

    admin_zone start

--- flask_app/admin_zone/admin_functions.py
+++ flask_app/admin_zone/admin_functions.py
@@ -134,6 +134,7 @@ def create_price(request):
 
         new_price = json.dumps(new_price, default=lambda x: x.__dict__)
         print(new_price)
+        print(type(new_price))
         prices.insert_one(new_price)
 
     except Exception as error:
