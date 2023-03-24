e122c3a562dcafb5088bf77f3f931eb1a8b690b7
Mr.Fill <mr.filldev@gmail.com>
2023-03-24 19:29:30 +0300

    admin_zone start

--- flask_app/admin_zone/admin_functions.py
+++ flask_app/admin_zone/admin_functions.py
@@ -141,9 +141,9 @@ def edit_price(request, price_id):
     print('2')
     print('old_carwash: ', price_id)
     categoryPrice = []
-    for i in list(CategoryAuto):
-        print(i)
-        categoryPrice.append(CostIdSum(i.name, form[str(i.name)]))
+    for category in list(CategoryAuto):
+        print(category)
+        categoryPrice.append(CostIdSum(category.name, form[str(category.name)]))
     data = json.dumps(categoryPrice, default=lambda x: x.__dict__)
     categoryPrice = json.loads(data)  # , object_hook=lambda d: SimpleNamespace(**d))
     set_fields = {'$set': {
