b86f7e7917266c3ed064e1454128c27a715a4d19
Mr.Fill <mr.filldev@gmail.com>
2023-03-27 19:18:24 +0300

    fix

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -486,8 +486,7 @@ def carwash_detail(carwash_id):
 
     enum_list = list(CategoryAuto)
     print(enum_list)
-    print('carwash_obj ', carwash_obj.Price)
-    print('carwash_obj ', carwash_obj.Price[1].Id)
+    print('carwash_obj ', carwash_obj)
     context = {
         'carwash': carwash_obj,
         ''
