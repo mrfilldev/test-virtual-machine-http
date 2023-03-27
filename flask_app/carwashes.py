c5a6e39dd5b25d2e3ec25b96658270e3c8bc7f88
Mr.Fill <mr.filldev@gmail.com>
2023-03-27 14:41:11 +0300

    creating carwash with prices

--- flask_app/carwashes.py
+++ flask_app/carwashes.py
@@ -135,8 +135,8 @@ def create_carwash_obj(request):
     new_carwash_dict = json.loads(new_carwash_json)  # , object_hook=lambda d: SimpleNamespace(**d))
     print('TYPE: ', type(new_carwash_dict))
     print('data: ', new_carwash_dict)
-    #res = Config.col_carwashes.insert_one(new_carwash_dict)
-    #print('WRITED CARWASH: ', res)
+    res = Config.col_carwashes.insert_one(new_carwash_dict)
+    print('WRITED CARWASH: ', res)
 
 
 def update_carwash_obj(request, carwash_id):
