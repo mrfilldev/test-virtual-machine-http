c08141b3ce1beb3d96a83dd0e224065341839844
Mr.Fill <mr.filldev@gmail.com>
2023-03-27 19:51:45 +0300

    fix

--- flask_app/carwashes.py
+++ flask_app/carwashes.py
@@ -193,7 +193,7 @@ def create_price_obj(request, carwash_id):
 
 
 def delete_carwash_obj(carwash_id):
-    db_carwashes.delete_one({'id': carwash_id})
+    db_carwashes.delete_one({'Id': carwash_id})
 
 
 def carwash_list_main():
