c38eab9f0dbe8fd5fff946332bb2b090aa6703f2
Mr.Fill <mr.filldev@gmail.com>
2023-03-28 20:27:57 +0300

    fix

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -139,7 +139,7 @@ async def user_canceled(order):
 
 
 async def make_some_noize(order):
-    order['_id'] = str.encode(order['Id'])
+    order['_id'] = str(str.encode(order['Id']))
     order = order.pop('Id')
 
     await write_into_db(order)
