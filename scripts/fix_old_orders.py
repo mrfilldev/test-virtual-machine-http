0c858301d93ddba569ffd2838e952070782f6ba3
Mr.Fill <mr.filldev@gmail.com>
2023-03-28 18:09:16 +0300

    fix

---
+++ scripts/fix_old_orders.py
@@ -0,0 +1,22 @@
+import json
+from types import SimpleNamespace
+
+from bson import json_util
+
+from config.config import Config
+
+orders = Config.col_orders.find({})
+
+for i in orders:
+    order_dict = json.loads(json_util.dumps(i))
+    carwash_obj = Config.db_carwashes.find_one({'Id': int(order_dict['CarWashId'])})  # dict
+    data = json.loads(json_util.dumps(carwash_obj))
+    value = carwash_obj.Name
+
+    set_command = {
+        "$set": {
+            "name_of_carwash": str(value),
+        },
+    }
+    orders.update_one({'Id': order_dict['Id']}, set_command)
+
