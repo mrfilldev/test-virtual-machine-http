2cc6549fddf4226c0a56e65be89ab139557ad484
Mr.Fill <mr.filldev@gmail.com>
2023-03-29 03:38:22 +0300

    fix

--- flask_app/classes_of_project.py
+++ flask_app/classes_of_project.py
@@ -12,3 +12,17 @@ class Order:
         self.SumPaidStationCompleted = SumCompleted
         self.SumCompleted = SumPaidStationCompleted
 
+
+class Carwash:
+    def __init__(self, _id, enable, name, address, Location: Point,
+                 Type, stepCost, limitMinCost, Boxes, Price):
+        self.Id = _id
+        self.Enable = enable
+        self.Name = name
+        self.Address = address
+        self.Location = Location
+        self.Type = Type
+        self.StepCost = stepCost
+        self.LimitMinCost = limitMinCost
+        self.Boxes = Boxes
+        self.Price = Price
