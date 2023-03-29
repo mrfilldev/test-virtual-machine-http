92a2086594eac04278832fb5a07ad542aa65cd66
Mr.Fill <mr.filldev@gmail.com>
2023-03-29 19:58:34 +0300

    fix

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -259,7 +259,14 @@ def add_company():
 @admin_status_required
 def admin_add_network():
     add_network(request)
-    return render_template('admin_zone/creations/create_network.html')
+    return render_template('admin_zone/networks/create_network.html')
+
+@app.route('/add_network/', methods=['POST', 'GET'])
+@admin_status_required
+def admin_networks():
+    list_networks(request)
+    return render_template('admin_zone/creations/list_networks.html')
+
 
 
 ########################################################################
