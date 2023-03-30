ece6d9272a2a2e1bf2f679d8a3505ea5de4806af
Mr.Fill <mr.filldev@gmail.com>
2023-03-30 15:00:21 +0300

    fix

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -18,7 +18,7 @@ import json
 from types import SimpleNamespace
 
 from flask_app import oauth_via_yandex
-from flask_app.admin_zone.admin_functions import check_root, admin_main, delete_user, test_view, \
+from admin_zone.admin_functions import check_root, admin_main, delete_user, test_view, \
     create_price, delete_price, edit_price, show_list_price, add_network, list_networks, add_user
 from flask_app.carwashes import create_carwash_obj, update_carwash_obj, carwash_list_main, CategoryAuto, \
     delete_carwash_obj
