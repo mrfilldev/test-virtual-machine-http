ac9c14581c071e29806cd64efcd27b9edd0ce588
Mr.Fill <mr.filldev@gmail.com>
2023-03-30 16:40:44 +0300

    fix

--- flask_app/urls.py
+++ flask_app/urls.py
@@ -5,6 +5,5 @@ from application import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret'
     app.config['SECRET_KEY'] = Config.SECRET_KEY
     app.run(host='127.0.0.1', port=8080)
