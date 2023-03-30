d4a1c8d7f868e233c8980be79743073d6c7093a4
Mr.Fill <mr.filldev@gmail.com>
2023-03-30 15:06:07 +0300

    fix

--- flask_app/classes_of_project.py
+++ flask_app/classes_of_project.py
@@ -1,6 +1,5 @@
 import json
 from types import SimpleNamespace
-from flask_app.urls import login_manager
 
 from bson import json_util
 from flask import session
@@ -10,8 +9,11 @@ from flask_app import oauth_via_yandex
 
 from flask_login import current_user, login_user, logout_user, login_required
 
+from flask_app.urls import app
+
 users = Config.col_users
-login_manager
+login = LoginManager(app)
+
 
 
 class Order:
