cded806e3d4f4dcbc4047da7083b1722dc0379af
Mr.Fill <mr.filldev@gmail.com>
2023-04-04 22:46:20 +0300

    fix

--- daemon/web_params/params.py
+++ daemon/web_params/params.py
@@ -47,12 +47,22 @@ class Config(object):
 
 
 class Sqs_params:
-    # from aws_requests_auth.aws_auth import AWSRequestsAuth
     client = boto3.client(
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        aws_access_key_id=Config.AWS_ACCESS_KEY_ID,  # os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY,  # os.getenv('AWS_SECRET_ACCESS_KEY'),
         service_name='sqs',
         endpoint_url='https://message-queue.api.cloud.yandex.net',
         region_name='ru-central1'
     )
     queue_url = client.create_queue(QueueName='test-tanker-carwsh-orders').get('QueueUrl')
+
+    # # from aws_requests_auth.aws_auth import AWSRequestsAuth
+    # client = boto3.client(
+    #     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    #     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    #     service_name='sqs',
+    #     endpoint_url='https://message-queue.api.cloud.yandex.net',
+    #     region_name='ru-central1'
+    # )
+    # queue_orders = 'https://message-queue.api.cloud.yandex.net/b1gjm9f9sf1pbis8lhhp/dj600000000bqnoc01b1/test-tanker-carwsh-orders'
+    # #queue_url = client.create_queue(QueueName='test-tanker-carwsh-orders').get('QueueUrl')
