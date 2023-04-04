2f888ca1a169e263db402fd48ff73fa5ab5f4a8c
Mr.Fill <mr.filldev@gmail.com>
2023-04-04 16:19:16 +0300

    fix

--- new_project/api/carwash_list.py
+++ new_project/api/carwash_list.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 
 from bson import json_util
 
-from new_project.database import database
+from ..database import database
 
 db_carwashes = database.col_carwashes
 db_prices = database.col_prices
