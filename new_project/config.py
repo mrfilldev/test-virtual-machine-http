00f08a4e3c46375f92a466c7da94d65291b18579
Mr.Fill <mr.filldev@gmail.com>
2023-04-04 16:30:41 +0300

    fix

--- new_project/config.py
+++ new_project/config.py
@@ -9,7 +9,15 @@ class Config(object):
     DEBUG = False
     # Включение защиты против "Cross-site Request Forgery (CSRF)"
     CSRF_ENABLED = True
-    SECRET_KEY = 'YOUR_RANDOM_SECRET_KEY'
+    URL_DEV = os.environ.get('URL_DEV')
+    API_KEY = os.environ.get('API_KEY')
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+    CHANNEL_ID = os.environ.get('CHANNEL_ID')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 
 class ProductionConfig(Config):
