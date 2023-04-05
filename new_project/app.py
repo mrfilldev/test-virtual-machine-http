8dcd6abb1305a0f40feb6648cb1356baafa762cf
Mr.Fill <mr.filldev@gmail.com>
2023-04-05 14:20:16 +0300

    fix

--- new_project/app.py
+++ new_project/app.py
@@ -21,4 +21,6 @@ app.register_blueprint(api_bp)
 app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    #app.config['SECRET_KEY'] = Config.SECRET_KEY
     app.run(host='127.0.0.1', port=8080)
--- old_project/flask_app/urls.py
+++ old_project/flask_app/urls.py
@@ -1,9 +1,9 @@
-from application import create_app
-from new_project.configuration.config import Config
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    app.config['SECRET_KEY'] = Config.SECRET_KEY
-    app.run(host='127.0.0.1', port=8080)
+# from application import create_app
+# from new_project.configuration.config import Config
+#
+# app = create_app()
+#
+# if __name__ == '__main__':
+#     app.secret_key = 'mysecret'
+#     app.config['SECRET_KEY'] = Config.SECRET_KEY
+#     app.run(host='127.0.0.1', port=8080)
