5928574ee9e29098e9cb8c190f8781014c1d237d
Mr.Fill <mr.filldev@gmail.com>
2023-04-05 15:05:27 +0300

    fix

--- new_project/main/main.py
+++ new_project/main/main.py
@@ -1,7 +1,14 @@
 import os
+
+import time
 import traceback
+from datetime import datetime, date
+
+
 from flask import Blueprint, request, Response, session, redirect, url_for, render_template
 from configuration.config import Config
+
+import oauth_via_yandex
 
 main_bp = Blueprint(
     'main_blueprint', __name__,
@@ -24,3 +31,52 @@ def index():
         return redirect(url_for('main'))
     else:
         return render_template('main/index.html')
+
+
+@app.route('/main')
+def main():
+    # get ya-token
+    try:
+        if 'ya-token' not in session:
+            resp = oauth_via_yandex.get_code(request)
+            for key in dict(session):
+                print(key, ":", session[key])
+            session['ya-token'] = resp['access_token']
+            print('ya-token has been inserted')
+        print('ya-token is True')
+        user_inf = oauth_via_yandex.get_user(session['ya-token'])
+        print('user_inf: ', user_inf)
+        user = Config.col_users.find_one({'_id': user_inf['id']})
+        print('user: ', user)
+        if user is None:
+            format = '%Y-%m-%dT%H:%M:%S%Z'
+            date_now = datetime.strptime(time.strftime(format, time.localtime()), format)
+            print(date_now)
+            print({
+                '_id': user_inf['id'],
+                'psuid': user_inf['psuid'],
+                'login': user_inf['login'],
+                'access_level': 'Новый пользователь',
+                'date_registered': str(date_now),
+                'company_name': '',
+                'inn': '',
+            })
+
+            # Config.col_users.insert_one(
+            #     {
+            #         '_id': user_inf['id'],
+            #         'psuid': user_inf['psuid'],
+            #         'login': user_inf['login'],
+            #         'access_level': 'Новый пользователь',
+            #         'date_registered': str(date_now),
+            #         'company_name': '',
+            #         'inn': '',
+            #     }
+            # )
+            print(f'user {user_inf["login"]} has been inserted')
+
+        return redirect(url_for('profile'))
+    except Exception as e:
+        traceback.print_exc()
+        print(f'EXEPTION: \n{type(Exception)}: e', e)  # добавить логгер
+        return "ошибОчка на стороне сервера :("
---
+++ new_project/main/oauth_via_yandex.py
@@ -0,0 +1,71 @@
+import json
+import requests
+import base64
+from configuration.config import Config
+
+
+def encode_base64(client_id, client_secret):
+    stroka = str(client_id) + ':' + str(client_secret)
+    encoded = base64.b64encode(stroka.encode("UTF-8"))
+    return encoded.decode("UTF-8")
+
+
+def get_code(request):
+    state = request.args.get('state')
+    print(state)
+
+    url = 'https://oauth.yandex.ru/token'
+
+    code = encode_base64(Config.YAN_CLIENT_ID, Config.YAN_CLIENT_SECRET)
+    print('code: ', code)
+    headers = {
+        'Authorization': 'Basic ' + encode_base64(Config.YAN_CLIENT_ID, Config.YAN_CLIENT_SECRET),
+    }
+
+    params = {
+        'grant_type': 'authorization_code',
+        'code': request.args.get('code'),
+    }
+
+    response = requests.post(url, data=params, headers=headers)
+    print('STATUS_CODE: ', response.status_code)
+    print("url:", url)
+    print("params:", params)
+    print('\nresponse: ', response)
+    print('\nresponse.content: ', response.content)
+    response = json.loads(response.content.decode('utf-8'))
+    print("response['access_token']:", response['access_token'])
+    print("response['expires_in']:", response['expires_in'])
+    print("response['refresh_token']:", response['refresh_token'])
+    print("response['token_type']:", response['token_type'])
+
+    return response
+
+
+#
+# curl -v -X POST 'https://oauth.yandex.ru/token' \
+# -H 'Content-type: application/x-www-form-urlencoded' \
+# -H 'Authorization: Basic MGM1NDZjZmFhNGU4NDBjNGI0MWJjYTRhOGFmMmU1NmE6Y2UxMGIxZjU0MTQxNGJhOWEwYmYzMTM5ZDBkZjlmNDk=' \
+# -d 'grant_type=authorization_code&code=6678635'
+def get_user(ya_token):
+    url = 'https://login.yandex.ru/info?'
+    params = {
+        'format': 'json',
+        'jwt_secret': 'secret_try'
+    }
+    headers = {
+        'Authorization': 'OAuth ' + ya_token,
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    print('STATUS_CODE: ', response.status_code)
+    print('content: ', response.content)
+    print("url:", url)
+    print("params:", params)
+    response = json.loads(response.content.decode('utf-8'))
+    return response
+
+#
+# curl -v GET 'https://login.yandex.ru/info?' \
+# -H 'Authorization: OAuth y0_AgAAAAAetcd8AAk1CgAAAADemu3gczbsmIFPTsuSiWY_l8Q0_STY3h8' \
+# -d 'format=json&jwt_secret=secret_try'
