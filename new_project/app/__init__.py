1be1a1b4dcc0dd94713a32f6dd382b1af1fbd6e5
Mr.Fill <mr.filldev@gmail.com>
2023-04-07 02:13:37 +0300

    flask login

--- new_project/app/__init__.py
+++ new_project/app/__init__.py
@@ -17,10 +17,7 @@ app.register_blueprint(api_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(profile_bp)
 
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    #app.config['SECRET_KEY'] = Config.SECRET_KEY
-    app.run(host='127.0.0.1', port=8080)
+
 
 
 
