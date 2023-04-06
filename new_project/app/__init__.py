6d54764d006c10e1e2295e8f1b57885d61435361
Mr.Fill <mr.filldev@gmail.com>
2023-04-07 01:46:13 +0300

    flask login

---
+++ new_project/app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+from . import models
+from . import configuration
+from .api.api import api_bp
+from .db import database
+
+from flask_login import LoginManager
+
+from .main.main import main_bp
+from .profile.profile import profile_bp
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='/static',
+    # template_folder='/templates'
+)
+app.CSRF_ENABLED = True
+app.DEBUG = False
+
+bootstrap = Bootstrap(app)
+login = LoginManager()
+app.register_blueprint(api_bp)
+app.register_blueprint(main_bp)
+app.register_blueprint(profile_bp)
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    #app.config['SECRET_KEY'] = Config.SECRET_KEY
+    app.run(host='127.0.0.1', port=8080)
+
+
+
+
