131ad31eb5dd0eb8ae8ac4878026ef320e900118
Mr.Fill <mr.filldev@gmail.com>
2023-04-07 02:38:43 +0300

    flask login

--- new_project/app/__init__.py
+++ new_project/app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 
 from flask_login import LoginManager
 
+from ..configuration.config import Config
 from ..api.api import api_bp
 from ..main.main import main_bp
 from ..profile.profile import profile_bp
@@ -16,11 +17,11 @@ login = LoginManager()
 app.register_blueprint(api_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(profile_bp)
+app.config['SECRET_KEY'] = Config.SECRET_KEY
 
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    #app.config['SECRET_KEY'] = Config.SECRET_KEY
-    app.run(host='127.0.0.1', port=8080)
+# if __name__ == '__main__':
+#     app.secret_key = 'mysecret'
+#     app.run(host='127.0.0.1', port=8080)
 
 
 
