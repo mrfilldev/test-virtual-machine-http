7578fa30a0c59ed04b878beaddf32b8d233d69e6
Mr.Fill <mr.filldev@gmail.com>
2023-04-07 02:07:39 +0300

    flask login

--- new_project/profile/profile.py
+++ new_project/profile/profile.py
@@ -1,7 +1,7 @@
 import os
 import traceback
 from flask import Blueprint, request, Response
-from configuration.config import Config
+from ..configuration.config import Config
 
 profile_bp = Blueprint(
     'profile_blueprint', __name__,
