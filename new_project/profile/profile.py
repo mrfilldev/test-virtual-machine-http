036903fbd0b6a35273f03a6bf540e019edfa3a16
Mr.Fill <mr.filldev@gmail.com>
2023-04-07 02:47:49 +0300

    flask login

--- new_project/profile/profile.py
+++ new_project/profile/profile.py
@@ -10,15 +10,17 @@ profile_bp = Blueprint(
 
 @profile_bp.route('/profile_safe')
 def profile_future_client():
-    #return 'Хотите стать клиентом - свяжитесь с нами'
+    # return 'Хотите стать клиентом - свяжитесь с нами'
     return render_template('profile/profile_future_client.html')
 
 
 @profile_bp.route('/profile_owner')
 def profile_owner():
-    return 'Вы наш клиент'
+    # return 'Вы наш клиент'
+    return render_template('profile/profile_owner.html')
 
 
 @profile_bp.route('/profile_owner')
 def profile_worker():
-    return 'Вы сотрудник мойки'
+    # return 'Вы сотрудник мойки'
+    return render_template('profile/profile_worker.html')
