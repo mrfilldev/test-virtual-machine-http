a545edc741f157a7c5831841bf4fc613e45915d4
Mr.Fill <mr.filldev@gmail.com>
2023-04-07 01:43:04 +0300

    flask login

--- new_project/app.py
+++ new_project/app.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-
-import app
 
 
 
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    #app.config['SECRET_KEY'] = Config.SECRET_KEY
-    app.run(host='127.0.0.1', port=8080)
