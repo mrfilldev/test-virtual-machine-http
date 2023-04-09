8259855b4584fd540b9273c84ef20328e73149e1
Mr.Fill <mr.filldev@gmail.com>
2023-04-09 18:42:00 +0300

    remake on flask login

--- new_project/main/main.py
+++ new_project/main/main.py
@@ -83,8 +83,7 @@ def main():
                     }
                 )
                 print(f'user {user_inf["login"]} has been inserted')
-
-            return redirect(url_for('profile_blueprint.profile_future_client'))
+                return redirect(url_for('profile_blueprint.profile_future_client'))
         else:
             return redirect(url_for('profile_blueprint.profile_owner'))
     except Exception as e:
