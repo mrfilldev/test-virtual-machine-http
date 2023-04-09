02031fd8f9bf470a5f65f005b67fa04f620e19a9
Mr.Fill <mr.filldev@gmail.com>
2023-04-09 13:14:27 +0300

    remake on flask login

--- new_project/profile/profile.py
+++ new_project/profile/profile.py
@@ -13,8 +13,8 @@ profile_bp = Blueprint(
 @profile_bp.route('/profile_safe')
 def profile_future_client():
     # return 'Хотите стать клиентом - свяжитесь с нами'
-    print(g)
-    print(type(g))
+    # print(g)
+    # print(type(g))
     return render_template('profile/profile_future_client.html')
 
 
@@ -24,7 +24,7 @@ def profile_owner():
     return render_template('profile/profile_owner.html')
 
 
-@profile_bp.route('/profile_owner')
+@profile_bp.route('/profile_worker')
 def profile_worker():
     # return 'Вы сотрудник мойки'
     return render_template('profile/profile_worker.html')
