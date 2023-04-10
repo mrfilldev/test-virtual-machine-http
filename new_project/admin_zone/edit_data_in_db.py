903aff6d37d914a7c143bec70cebbe473d289082
Mr.Fill <mr.filldev@gmail.com>
2023-04-10 20:26:05 +0300

    remake on flask login

--- new_project/admin_zone/edit_data_in_db.py
+++ new_project/admin_zone/edit_data_in_db.py
@@ -1,9 +1,16 @@
 from flask import render_template
 
-from ..db.database import db_test
+from ..db.database import db_test, client
 
 
 def list_all_cols_in_db():
+    for db_name in client.list_database_names():
+        db = client[db_name]
+        for coll_name in db.list_collection_names():
+            print("db: {}, collection:{}".format(db_name, coll_name))
+            for r in db[coll_name].find({}):
+                print(r)
+            print('\n\n')
     list_of_cols = db_test.collection_names()
     print(list_of_cols)
     context = {'list': list_of_cols}
