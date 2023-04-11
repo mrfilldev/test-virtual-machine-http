96fbc4b2cbc40992fe607b9b6ff22118fc6074d4
Mr.Fill <mr.filldev@gmail.com>
2023-04-11 16:17:29 +0300

    carwashes view

--- new_project/admin_zone/manage_prices.py
+++ new_project/admin_zone/manage_prices.py
@@ -108,3 +108,4 @@ def edit_price(request, price_id):
 def delete_price(price_id):
     database.col_prices.delete_one({'Id': price_id})
     print('deleted price: ', price_id)
+    return redirect(url_for('admin_blueprint.list_of_prices'))
