2e8445a748fc323ba373a8e3afac6de1ec1a40fb
Mr.Fill <mr.filldev@gmail.com>
2023-04-11 14:24:24 +0300

    remake on flask login

--- new_project/app/__init__.py
+++ new_project/app/__init__.py
@@ -8,6 +8,7 @@ from ..configuration.config import Config
 from ..api.api import api_bp
 from ..main.main import main_bp
 from ..profile.profile import profile_bp
+from ..view_carwash.carwash import carwash_bp
 
 app = Flask(__name__, template_folder='templates')
 app.CSRF_ENABLED = True
@@ -20,6 +21,7 @@ app.register_blueprint(api_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(profile_bp)
+app.register_blueprint(carwash_bp)
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 
 if __name__ == '__main__':
