a9953b68957f2883a8b1a823257aa7a4eb13b8ea
Mr.Fill <mr.filldev@gmail.com>
2023-04-19 14:54:41 +0300

    fix

--- new_project/admin_zone/fix_buttons.py
+++ new_project/admin_zone/fix_buttons.py
@@ -22,11 +22,12 @@ def fix_network_id_in_orders():
         if current_carwash_obj == None:
             database.col_orders.delete_one({'_id': order_obj._id})
             print('deleted order_id:', order_obj._id)
-        print('current_carwash_obj: ', current_carwash_obj)
-        old_order = {'_id': order_obj._id}
-        set_fields = {'$set': {
-            'network_id': current_carwash_obj.network_id,
-        }}
-        new_order = database.col_orders.update_one(old_order, set_fields)
+        else:
+            print('current_carwash_obj: ', current_carwash_obj)
+            old_order = {'_id': order_obj._id}
+            set_fields = {'$set': {
+                'network_id': current_carwash_obj.network_id,
+            }}
+            new_order = database.col_orders.update_one(old_order, set_fields)
 
     return redirect(url_for('admin_blueprint.admin_main'))
