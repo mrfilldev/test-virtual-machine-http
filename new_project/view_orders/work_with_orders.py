e6f559b8031e8f2e47e1bfda7ab630696b7a13a2
Mr.Fill <mr.filldev@gmail.com>
2023-04-19 13:41:37 +0300

    fix

--- new_project/view_orders/work_with_orders.py
+++ new_project/view_orders/work_with_orders.py
@@ -13,70 +13,72 @@ queue_url = Sqs_params.queue_url
 
 
 def list_orders(g):
-    if 'networks' in g.user_db or g.user_db.role == 'admin':
+    if 'networks' in g.user_db:
         network = g.user_db['networks'][0]
         print('network:', network)
         all_carwashes = database.col_carwashes.find({'network_id': network})
         print('network:', all_carwashes)
-        carwashes = []
-        for i in all_carwashes:
-            data = json.loads(json_util.dumps(i))
-            data = json.dumps(data, default=lambda x: x.__dict__)
-            carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-            print('carwash_obj:', carwash_obj)
-            carwashes.append(carwash_obj._id)
-        print('carwashes: ', carwashes)
-        orders_of_network = database.col_orders.find({'CarWashId': {'$in': carwashes}}).limit(20)
-        print('orders_of_network:', orders_of_network)
+    elif g.user_db.role == 'admin':
+        all_carwashes = database.col_carwashes.find({})
+    else:
+        abort(404)
+    carwashes = []
+    for i in all_carwashes:
+        data = json.loads(json_util.dumps(i))
+        data = json.dumps(data, default=lambda x: x.__dict__)
+        carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        print('carwash_obj:', carwash_obj)
+        carwashes.append(carwash_obj._id)
+    print('carwashes: ', carwashes)
+    orders_of_network = database.col_orders.find({'CarWashId': {'$in': carwashes}}).limit(20)
+    print('orders_of_network:', orders_of_network)
 
-        orders_list = []
-        count_orders = 0
-        distinctCarwashId = []
-        for count_orders, i in enumerate(list(orders_of_network)[::-1], 1):
-            # count_orders += 1
-            print(i)
-            data = json.loads(json_util.dumps(i))
-            data = json.dumps(data, default=lambda x: x.__dict__)
-            # order_obj = json.loads(data, object_hook=lambda d: Order(**d))
-            order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-            orders_list.append(order_obj)
+    orders_list = []
+    count_orders = 0
+    distinctCarwashId = []
+    for count_orders, i in enumerate(list(orders_of_network)[::-1], 1):
+        # count_orders += 1
+        print(i)
+        data = json.loads(json_util.dumps(i))
+        data = json.dumps(data, default=lambda x: x.__dict__)
+        # order_obj = json.loads(data, object_hook=lambda d: Order(**d))
+        order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        orders_list.append(order_obj)
 
-            if hasattr(order_obj, 'CarWashId') and order_obj.CarWashId not in distinctCarwashId:
-                distinctCarwashId.append(order_obj.CarWashId)
+        if hasattr(order_obj, 'CarWashId') and order_obj.CarWashId not in distinctCarwashId:
+            distinctCarwashId.append(order_obj.CarWashId)
 
-        # mongo find by filter in () // projections
-        carwashes_names = []
-        carwashes = database.col_carwashes.find({"_id": {"$in": distinctCarwashId}}, {"_id": 1, "Name": 1})
-        for i in carwashes:
-            data = json.loads(json_util.dumps(i))
-            data = json.dumps(data, default=lambda x: x.__dict__)
-            carwash = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-            carwashes_names.append(carwash)
+    # mongo find by filter in () // projections
+    carwashes_names = []
+    carwashes = database.col_carwashes.find({"_id": {"$in": distinctCarwashId}}, {"_id": 1, "Name": 1})
+    for i in carwashes:
+        data = json.loads(json_util.dumps(i))
+        data = json.dumps(data, default=lambda x: x.__dict__)
+        carwash = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        carwashes_names.append(carwash)
 
-        request_xhr_key = request.headers.get('X-Requested-With')
-        if request_xhr_key == 'XMLHttpRequest':
-            context = {
-                'orders_list': orders_list,
-                'count_orders': count_orders,
-                'carwashes': carwashes_names,
-            }
-            return render_template('orders/orders_table.html', context=context)
-
-
-        today = date.today()
+    request_xhr_key = request.headers.get('X-Requested-With')
+    if request_xhr_key == 'XMLHttpRequest':
         context = {
             'orders_list': orders_list,
             'count_orders': count_orders,
             'carwashes': carwashes_names,
-            'date': today,
         }
-        return render_template(
-            'orders/orders_list.html',
-            context=context
-        )
-    else:
-        all_carwashes = database.col_carwashes.find({'network_id': network})
-        return abort(404)
+        return render_template('orders/orders_table.html', context=context)
+
+
+    today = date.today()
+    context = {
+        'orders_list': orders_list,
+        'count_orders': count_orders,
+        'carwashes': carwashes_names,
+        'date': today,
+    }
+    return render_template(
+        'orders/orders_list.html',
+        context=context
+    )
+
 
 
 def owner_order_detail(order_id):
