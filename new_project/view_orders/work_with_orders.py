8f3f7d1869c7bca088d2f072c8c50cf9a219b172
Mr.Fill <mr.filldev@gmail.com>
2023-04-19 16:11:15 +0300

    fix

--- new_project/view_orders/work_with_orders.py
+++ new_project/view_orders/work_with_orders.py
@@ -16,41 +16,27 @@ def list_orders(g):
     if 'networks' in g.user_db:
         network = g.user_db['networks'][0]
         print('network:', network)
-        all_carwashes = database.col_carwashes.find({'network_id': network})
-        print('network:', all_carwashes)
+        orders = database.col_orders.find({'network_id': network})
     elif g.user_db['role'] == 'admin':
-        all_carwashes = database.col_carwashes.find({})
+        orders = database.col_orders.find({})
     else:
         return abort(404)
-    carwashes = []
-    for i in all_carwashes:
-        data = json.loads(json_util.dumps(i))
-        data = json.dumps(data, default=lambda x: x.__dict__)
-        carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-        print('carwash_obj:', carwash_obj)
-        carwashes.append(carwash_obj._id)
-    print('carwashes: ', carwashes)
-    orders_of_network = database.col_orders.find({'CarWashId': {'$in': carwashes}})
-    print('orders_of_network:', orders_of_network)
-
     orders_list = []
     count_orders = 0
     distinctCarwashId = []
-    for count_orders, i in enumerate(list(orders_of_network)[::-1], 1):
-        # count_orders += 1
-        print(i)
+    for count_orders, i in enumerate(list(orders)[::-1], 1):
         data = json.loads(json_util.dumps(i))
         data = json.dumps(data, default=lambda x: x.__dict__)
-        # order_obj = json.loads(data, object_hook=lambda d: Order(**d))
         order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        print('order_obj:', order_obj)
         orders_list.append(order_obj)
 
-        if hasattr(order_obj, 'CarWashId') and order_obj.CarWashId not in distinctCarwashId:
+        if order_obj.CarWashId not in distinctCarwashId:
             distinctCarwashId.append(order_obj.CarWashId)
 
     # mongo find by filter in () // projections
     carwashes_names = []
-    carwashes = database.col_carwashes.find({"_id": {"$in": distinctCarwashId}}, {"_id": 1, "Name": 1})
+    carwashes = database.col_carwashes.find({"_id": {"$in": distinctCarwashId}})
     for i in carwashes:
         data = json.loads(json_util.dumps(i))
         data = json.dumps(data, default=lambda x: x.__dict__)
@@ -66,7 +52,6 @@ def list_orders(g):
         }
         return render_template('orders/orders_table.html', context=context)
 
-
     today = date.today()
     context = {
         'orders_list': orders_list,
@@ -78,7 +63,6 @@ def list_orders(g):
         'orders/orders_list.html',
         context=context
     )
-
 
 
 def owner_order_detail(order_id):
