2bb687d2d95836e42e77cf089b9912ede0b3f5b9
Mr.Fill <mr.filldev@gmail.com>
2023-04-19 14:56:57 +0300

    fix

--- new_project/admin_zone/fix_buttons.py
+++ new_project/admin_zone/fix_buttons.py
@@ -15,19 +15,23 @@ def fix_network_id_in_orders():
         data = json.dumps(data, default=lambda x: x.__dict__)
         order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
         print('order_obj: ', order_obj)
-        current_carwash = database.col_carwashes.find_one({'_id': order_obj.CarWashId})
-        data = json.loads(json_util.dumps(current_carwash))
-        data = json.dumps(data, default=lambda x: x.__dict__)
-        current_carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-        if current_carwash_obj == None:
+        if 'CarWashId' in order_obj:
+            current_carwash = database.col_carwashes.find_one({'_id': order_obj.CarWashId})
+            data = json.loads(json_util.dumps(current_carwash))
+            data = json.dumps(data, default=lambda x: x.__dict__)
+            current_carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+            if current_carwash_obj == None:
+                database.col_orders.delete_one({'_id': order_obj._id})
+                print('deleted order_id:', order_obj._id)
+            else:
+                print('current_carwash_obj: ', current_carwash_obj)
+                old_order = {'_id': order_obj._id}
+                set_fields = {'$set': {
+                    'network_id': current_carwash_obj.network_id,
+                }}
+                new_order = database.col_orders.update_one(old_order, set_fields)
+        else:
             database.col_orders.delete_one({'_id': order_obj._id})
             print('deleted order_id:', order_obj._id)
-        else:
-            print('current_carwash_obj: ', current_carwash_obj)
-            old_order = {'_id': order_obj._id}
-            set_fields = {'$set': {
-                'network_id': current_carwash_obj.network_id,
-            }}
-            new_order = database.col_orders.update_one(old_order, set_fields)
 
     return redirect(url_for('admin_blueprint.admin_main'))
