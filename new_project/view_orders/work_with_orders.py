c297ae253528fc293b9de9488d7983bd8a7b263f
Mr.Fill <mr.filldev@gmail.com>
2023-04-20 15:41:15 +0300

    fix

--- new_project/view_orders/work_with_orders.py
+++ new_project/view_orders/work_with_orders.py
@@ -51,7 +51,12 @@ def list_orders(g, skip=0, limit=25):
         data = json.dumps(data, default=lambda x: x.__dict__)
         carwash = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
         carwashes_names.append(carwash)
-    limit = limit + 25
+
+    if (orders_count - limit) <= 25:
+        limit = orders_count - limit
+    else:
+        limit = limit + 25
+
     request_xhr_key = request.headers.get('X-Requested-With')
     if request_xhr_key == 'XMLHttpRequest':
         context = {
