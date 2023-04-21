9f55cce3ca3af1005fe54c22127f27b64c04f43b
Mr.Fill <mr.filldev@gmail.com>
2023-04-21 15:06:46 +0300

    fix

---
+++ new_project/monitoring/monitoring.py
@@ -0,0 +1,34 @@
+import json
+import os
+import traceback
+from types import SimpleNamespace
+
+from bson import json_util
+from flask import Blueprint, request, Response, render_template, g, session, redirect, url_for
+
+from ..configuration.config import Config
+
+from flask_login import current_user
+
+from ..db import database
+from ..main import oauth_via_yandex
+
+monitoring_bp = Blueprint(
+    'monitoring_blueprint', __name__,
+)
+
+
+@monitoring_bp.before_request
+def load_user():
+    user_inf = oauth_via_yandex.get_user(session['ya-token'])
+    g.user_inf = user_inf
+    print('g.user_inf: ', g.user_inf)
+    user = database.col_users.find_one({'_id': user_inf['id']})
+    g.user_db = user
+    print('g.user_db: :', g.user_db)
+
+
+@monitoring_bp.route('/monitoring_carwash/', methods=['POST', 'GET'])
+def monitoring():
+    return monitoring(g)
+
---
+++ new_project/monitoring/monitoring_backend.py
@@ -0,0 +1,47 @@
+import json
+from types import SimpleNamespace
+
+from bson import json_util
+from flask import render_template
+
+from ..db import database
+from ..db.models import Types
+
+
+def get_carwashes(g_user_flask):
+    if 'networks' not in g_user_flask.user_db:
+        all_carwashes = database.col_carwashes.find({})
+        network_obj = None
+    else:
+        network = g_user_flask.user_db['networks'][0]
+        all_carwashes = database.col_carwashes.find({'network_id': network})
+        print('network:', network)
+        network = database.col_networks.find({'_id': network})
+        data = json.loads(json_util.dumps(network))
+        data = json.dumps(data, default=lambda x: x.__dict__)
+        network_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))[0]  # SimpleNamespace
+        print('network_obj:', network_obj)
+
+    carwashes_list = []
+    count_carwashes = 0
+
+    for count_carwashes, i in enumerate(list(all_carwashes)[::-1], 1):
+        data = json.loads(json_util.dumps(i))
+        data = json.dumps(data, default=lambda x: x.__dict__)
+        carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        carwashes_list.append(carwash_obj)
+        print(carwash_obj)
+    print(carwashes_list)
+    return carwashes_list
+
+
+def monitoring(g_user_flask):
+
+    context = {
+        'carwashes_list': get_carwashes(g_user_flask),
+        'enum_type_list': print(list(Types)),
+    }
+    return render_template(
+        'monitoring/all_carwashes.html',
+        context=context
+    )
