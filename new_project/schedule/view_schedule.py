b759399a57cc38790994afa3ef6c11c699b6bf4d
Mr.Fill <mr.filldev@gmail.com>
2023-04-25 14:44:49 +0300

    fix

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -1,8 +1,21 @@
+from datetime import datetime, timedelta
+
 from flask import render_template
 
 
-def view_schedule(g_user_flask):
-    context = {
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
 
+
+def view_schedule(g_user_flask):
+    dts = [dt.strftime('%Y-%m-%d T%H:%M Z') for dt in
+           datetime_range(datetime(2023, 4, 2, 44), datetime(2023, 4, 2, 44 + 12),
+                          timedelta(minutes=15))]
+
+    context = {
+        'dts': dts,
     }
     return render_template('schedule/view_schedule.html', context=context)
