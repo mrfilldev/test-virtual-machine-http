9c6f17033004dd63cf5ec12f9aaf972bb70b198e
Mr.Fill <mr.filldev@gmail.com>
2023-04-27 16:01:00 +0300

    fix

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -16,11 +16,11 @@ def view_schedule(g_user_flask):
     events = [
         {
             'title': 'event1',
-            'date': '2021-10-15'
+            'date': '2023-4-27'
         },
         {
             'title': 'event2',
-            'date': '2021-10-20'
+            'date': '2021-4-27'
         }
     ]
     # Pass schedule data to template
