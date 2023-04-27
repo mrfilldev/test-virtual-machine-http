9919f869f78782bc1f3e5a124f5bbad646b15b1f
Mr.Fill <mr.filldev@gmail.com>
2023-04-27 13:15:04 +0300

    fix

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -13,35 +13,62 @@ def datetime_range(start, end, delta):
 
 
 def view_schedule(g_user_flask):
-    dts = [dt.strftime('%Y-%m-%d T%H:%M Z') for dt in
-           datetime_range(datetime(year=2023, month=4, day=25, hour=14),
-                          datetime(year=2023, month=4, day=25, hour=14 + 9),
-                          timedelta(minutes=15))]
-    list_test_orders = []
-    new_order = TestScheduleOrder(
-        1700, 5, 45
-    )
-    list_test_orders.append(new_order)
-    new_order = TestScheduleOrder(
-        2200, 4, 90
-    )
-    list_test_orders.append(new_order)
-    new_order = TestScheduleOrder(
-        3300, 5, 120
-    )
-    list_test_orders.append(new_order)
-
-    list_test_orders = [
-        'Кат.5 2350&#8381',
-        '',
-        'Кат.3 1200&#8381',
-        '',
-        'Кат.4 350&#8381',
-        ''
+    schedule_data = [
+        {
+            "title": "All Day Event",
+            "start": "2014-09-01"
+        },
+        {
+            "title": "Long Event",
+            "start": "2014-09-07",
+            "end": "2014-09-10"
+        },
+        {
+            "id": "999",
+            "title": "Repeating Event",
+            "start": "2014-09-09T16:00:00-05:00"
+        },
+        {
+            "id": "999",
+            "title": "Repeating Event",
+            "start": "2014-09-16T16:00:00-05:00"
+        },
+        {
+            "title": "Conference",
+            "start": "2014-09-11",
+            "end": "2014-09-13"
+        },
+        {
+            "title": "Meeting",
+            "start": "2014-09-12T10:30:00-05:00",
+            "end": "2014-09-12T12:30:00-05:00"
+        },
+        {
+            "title": "Lunch",
+            "start": "2014-09-12T12:00:00-05:00"
+        },
+        {
+            "title": "Meeting",
+            "start": "2014-09-12T14:30:00-05:00"
+        },
+        {
+            "title": "Happy Hour",
+            "start": "2014-09-12T17:30:00-05:00"
+        },
+        {
+            "title": "Dinner",
+            "start": "2014-09-12T20:00:00"
+        },
+        {
+            "title": "Birthday Party",
+            "start": "2014-09-13T07:00:00-05:00"
+        },
+        {
+            "title": "Click for Google",
+            "url": "http://google.com/",
+            "start": "2014-09-28"
+        }
     ]
-    context = {
-        'dts': dts,
-        'date': date.today(),
-        'list_test_orders': list_test_orders,
-    }
-    return render_template('schedule/view_schedule.html', context=context)
+
+    # Pass schedule data to template
+    return render_template('schedule/view_schedule.html', schedule_data=schedule_data)
