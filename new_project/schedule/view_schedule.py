da9a481ee719969c116d1f00ce2ba468d3dae03f
Mr.Fill <mr.filldev@gmail.com>
2023-05-04 18:09:38 +0300

    calendar

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -29,51 +29,23 @@ def get_orders(carwash_id):  # 7810324c8fea4af8bc3c3d6776cfc494
             'title': 'Заказ',
             'start': order_obj.DateCreate.replace('Z', ''),
             'end': order_obj.DateCreate.replace('Z', ''),
-            'resourceId': (chr(ord('`')+int(order_obj.BoxNumber))),
+            'resourceId': (chr(ord('`') + int(order_obj.BoxNumber))),
             'carNumber': order_obj.ContractId,
         })
     print(events_list)
     return events_list
 
 
-def view_schedule(g_user_flask):
-    # if 'networks' in g_user_flask.user_db:
-    #     network = g_user_flask.user_db['networks'][0]
-    #     print('network:', network)
-    #
-    #     search = {'network_id': network}
-    # elif g_user_flask.user_db['role'] == 'admin':
-    #     search = {}
-    # Pass schedule data to template
-    resources = [
-        {'id': 'a', 'title': 'Бокс 1'},
-        {'id': 'b', 'title': 'Бокс 2'},
-        {'id': 'c', 'title': 'Бокс 3'},
-        {'id': 'd', 'title': 'Бокс 4'},
-        {'id': 'e', 'title': 'Бокс 5'},
-        {'id': 'f', 'title': 'Бокс 6'},
-        {'id': 'g', 'title': 'Бокс 7'},
-        {'id': 'h', 'title': 'Бокс 8'},
-        {'id': 'i', 'title': 'Бокс 9'},
-        {'id': 'j', 'title': 'Бокс 10'},
-        {'id': 'k', 'title': 'Бокс 11'},
-        {'id': 'l', 'title': 'Бокс 12'},
-    ]
-    carwash_start_time = '08:00:00'
-    carwash_end_time = '23:00:00'
-    date_today = datetime.today().strftime('%Y-%m-%d')
-    now_iso = datetime.now().isoformat()
-
-    # events = get_orders('7810324c8fea4af8bc3c3d6776cfc494')
-    context = {
-        # 'orders': events,
-        'boxes': resources,
-        'carwash_start_time': carwash_start_time,
-        'carwash_end_time': carwash_end_time,
-        'date_today': date_today,
-        'now_iso': now_iso
-    }
-    return render_template('schedule/view_schedule.html', context=context)
+def get_boxes(carwash_obj):
+    resources = []
+    for i in range(len(carwash_obj.Boxes)):
+        resources.append(
+            {
+                'id': chr(ord('`') + i),
+                'title': f'Бокс {i}',
+            }
+        )
+    return resources
 
 
 def get_carwash_obj(carwash_id):
@@ -88,20 +60,7 @@ def get_carwash_obj(carwash_id):
 def view_schedule_of_certain_carwash(carwash_id, g_user_flask):
     carwash_obj = get_carwash_obj(carwash_id)
 
-    resources = [
-        {'id': 'a', 'title': 'Бокс 1'},
-        {'id': 'b', 'title': 'Бокс 2'},
-        {'id': 'c', 'title': 'Бокс 3'},
-        {'id': 'd', 'title': 'Бокс 4'},
-        {'id': 'e', 'title': 'Бокс 5'},
-        {'id': 'f', 'title': 'Бокс 6'},
-        {'id': 'g', 'title': 'Бокс 7'},
-        {'id': 'h', 'title': 'Бокс 8'},
-        {'id': 'i', 'title': 'Бокс 9'},
-        {'id': 'j', 'title': 'Бокс 10'},
-        {'id': 'k', 'title': 'Бокс 11'},
-        {'id': 'l', 'title': 'Бокс 12'},
-    ]
+    resources = get_boxes(carwash_obj)
     carwash_start_time = '08:00:00'
     carwash_end_time = '23:00:00'
     date_today = datetime.today().strftime('%Y-%m-%d')
