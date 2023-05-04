9caa25d3ff11f19d86fd8e8c2dc097c81b120826
Mr.Fill <mr.filldev@gmail.com>
2023-05-04 20:52:26 +0300

    calendar

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -27,8 +27,8 @@ def get_orders(carwash_id):  # 7810324c8fea4af8bc3c3d6776cfc494
         print('order_obj:', order_obj)
         events_list.append({
             'title': 'Заказ',
-            'start': print(str(datetime.strptime(order_obj.DateCreate.replace('Z', ''), "%Y-%m-%dT%H:%M:%S.%f").strftime("%Y-%m-%dT%H:%M"))),
-            #'start': print(order_obj.DateCreate.replace('Z', '').split('.')[0]),
+            #'start': print(str(datetime.strptime(order_obj.DateCreate.replace('Z', ''), "%Y-%m-%dT%H:%M:%S.%f").strftime("%Y-%m-%dT%H:%M"))),
+            'start': print(order_obj.DateCreate.replace('Z', '')),
             'end': print(order_obj.DateCreate.replace('Z', '')),
             'date': print(order_obj.DateStart),
             'resourceId': (chr(ord('`') + int(order_obj.BoxNumber))),
