94e8ee24a2f75d5a1f35f46e6104d085d0ebf893
Mr.Fill <mr.filldev@gmail.com>
2023-05-10 16:16:32 +0300

    calendar

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -45,7 +45,7 @@ def get_orders(carwash_id):  # 7810324c8fea4af8bc3c3d6776cfc494
             'resourceId': (chr(ord('`') + int(order_obj.BoxNumber))),
             'box': order_obj.BoxNumber,
             'carNumber': order_obj.CarNumber,
-            'category': 'Кат. - ' if '' else order_obj.Category,
+            'category': '-' if order_obj.Category == '' else order_obj.Category,
             'car_brand': order_obj.CarBrand,
             'car_model': order_obj.CarModel,
         })
