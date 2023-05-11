43716c2432975c47292bf7f4ee473efdcdf6a539
Mr.Fill <mr.filldev@gmail.com>
2023-05-11 14:45:04 +0300

    calendar

--- new_project/operational_panel/backend.py
+++ new_project/operational_panel/backend.py
@@ -1,2 +1,29 @@
+import json
+from types import SimpleNamespace
+
+from bson import json_util
+from flask import render_template
+
+from new_project.db import database
+
+
+def get_carwash_obj(carwash_id):
+    carwash_obj = database.col_carwashes.find_one({'_id': carwash_id})  # dict
+    data = json.loads(json_util.dumps(carwash_obj))
+    data = json.dumps(data, default=lambda x: x.__dict__)
+    carwash_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))  # SimpleNamespace
+    print('carwash_obj: ', carwash_obj)
+    return carwash_obj
+
+
 def view_boxes(request, carwash_id, g_flask_user):
-    return f"{request, carwash_id, g_flask_user}\nОперативная панель (боксы состояние)"
+    carwash_obj = get_carwash_obj(carwash_id)
+
+
+    context = {
+        'carwash_obj': carwash_obj,
+    }
+    return render_template('operation_panel/view_boxes.html', context=context)
+
+
+
