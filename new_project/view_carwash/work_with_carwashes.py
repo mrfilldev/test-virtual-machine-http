554a4a8ee9339eba93133df8502fed01e86c1e76
Mr.Fill <mr.filldev@gmail.com>
2023-05-15 12:57:01 +0300

    calendar

--- new_project/view_carwash/work_with_carwashes.py
+++ new_project/view_carwash/work_with_carwashes.py
@@ -185,7 +185,6 @@ def create_carwash_obj(request, g):
 
     context = {
         'create_new': True,
-        'carwash': None,
         'prices_list': prices_list,
         'count_prices': count_prices,
     }
