288edf5755fa68a7fa8116dfd1364788b9188241
Mr.Fill <mr.filldev@gmail.com>
2023-05-16 19:29:26 +0300

    fix

--- new_project/prices/main.py
+++ new_project/prices/main.py
@@ -37,7 +37,7 @@ def prices_list_table(carwash_id):
 
 
 @prices_bp.route('/price_info/<string:price_id>', methods=['GET'])
-def get_price_info_by_id(price_id):
+def get_price_info_by_id(price_id=''):
     return get_price_info(price_id)
 
 @prices_bp.app_template_filter()
