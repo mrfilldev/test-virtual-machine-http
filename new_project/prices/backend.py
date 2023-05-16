d670cae176aff70abb88f4fde4e6b3a9c99515f8
Mr.Fill <mr.filldev@gmail.com>
2023-05-16 15:50:34 +0300

    calendar

--- new_project/prices/backend.py
+++ new_project/prices/backend.py
@@ -2,6 +2,7 @@ import json
 from types import SimpleNamespace
 
 from bson import json_util
+from flask import render_template
 
 from ..db import database
 
@@ -16,3 +17,10 @@ def get_prices_obj_list():
         price_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
         prices_list.append(price_obj)
     return prices_list
+
+
+def show_prices_list(carwash_id):
+    context = {
+        'prices': get_prices_obj_list(),
+    }
+    return render_template('prices/price_list.html', context=context)
