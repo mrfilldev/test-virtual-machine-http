b2b814322de1911f6333ddb7e3622d2a3b8f922c
Mr.Fill <mr.filldev@gmail.com>
2023-05-24 18:02:54 +0300

    fix

--- new_project/view_carwash/work_with_carwashes.py
+++ new_project/view_carwash/work_with_carwashes.py
@@ -41,7 +41,6 @@ def create_prices(request, dict_of_form, update=False, carwash_id=None):
         all_prices = carwash_obj.Price
         print('all_prices:\n', all_prices)
 
-
     for j in dict_of_form:
         if 'price' in j:
             print(j.split('_'))
@@ -309,7 +308,8 @@ def update_carwash_obj(request, carwash_id):
 
     # update_cost_of_price(dict_of_form, carwash_id)
 
-    print('UPD_PRICE:', json.loads(json.dumps(create_prices(request, dict_of_form), default=lambda x: x.__dict__)))
+    print('UPD_PRICE:', json.loads(json.dumps(create_prices(request, dict_of_form, update=True, carwash_id=carwash_id),
+                                              default=lambda x: x.__dict__)))
 
     old_carwash = {'_id': carwash_id}
     print('old_carwash: ', old_carwash)
@@ -320,7 +320,7 @@ def update_carwash_obj(request, carwash_id):
         'Location': {'lat': form['lat'], 'lon': form['lon']},
         'Type': Types.SelfService.name,
         'Boxes': new_boxes_list_of_dict,
-        #'Price': json.loads(json.dumps(create_prices(request, dict_of_form, update=True, carwash_id=carwash_id), default=lambda x: x.__dict__)),
+        # 'Price': json.loads(json.dumps(create_prices(request, dict_of_form, update=True, carwash_id=carwash_id), default=lambda x: x.__dict__)),
         'CarwashAdmin': form['login_administrator'],
     }}
     new_carwash = database.col_carwashes.update_one(old_carwash, set_fields)
