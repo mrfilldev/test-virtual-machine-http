772e2ab47d1ff69495f331b1524b1a43c94804d3
Mr.Fill <mr.filldev@gmail.com>
2023-05-26 13:01:21 +0300

    fix

--- new_project/start-service.sh
+++ new_project/start-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export FLASK_RUN_HOST=127.0.0.1
 export FLASK_RUN_PORT=8080
-/home/mrfilldev/test-server/venv/bin/python3 python -m flask run
+/home/mrfilldev/test-server/venv/bin/python3 flask run
