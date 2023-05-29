45b319bb785160327e2e5be5036b8f2507b90ca9
Mr.Fill <mr.filldev@gmail.com>
2023-05-29 20:23:36 +0300

    fix

--- new_project/admin_zone/admin_profile.py
+++ new_project/admin_zone/admin_profile.py
@@ -4,7 +4,7 @@ from dateutil import parser
 from flask import render_template, request, Blueprint, session, g, url_for, redirect
 
 from .edit_data_in_db import list_all_cols_in_db
-from .fix_buttons import fix_network_id_in_orders, back_carwashes_refresh_prices, clear_sets
+from .fix_buttons import fix_network_id_in_orders, back_carwashes_refresh_prices, remake_prices_to_set
 from .manage_networks import list_networks, network_detail, add_network
 from .manage_prices import show_list_price, create_price, edit_price, delete_price
 from .manage_users import users_list_view, user_detail, delete_user
@@ -96,7 +96,7 @@ def admin_delete_price(price_id):
 
 @admin_bp.route('/clear_sets/', methods=['POST', 'GET'])
 def admin_clear_sets():
-    return clear_sets()
+    return remake_prices_to_set()
 
 
 @admin_bp.route('/carwashes_refresh_prices')
