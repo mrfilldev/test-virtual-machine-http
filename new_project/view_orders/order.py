c1a4aa1e84d39e04013fe598aae6446f50d7bc7c
Mr.Fill <mr.filldev@gmail.com>
2023-05-31 14:15:09 +0300

    fix

--- new_project/view_orders/order.py
+++ new_project/view_orders/order.py
@@ -30,7 +30,7 @@ def load_user():
 
 
 @order_bp.route('/orders_list/', methods=['POST', 'GET'])
-@order_bp.route('/orders_list//<int:p>', methods=['POST', 'GET'])
+@order_bp.route('/orders_list/<int:p>', methods=['POST', 'GET'])
 @order_bp.route('/orders_list/<string:carwash_id>/<int:p>', methods=['POST', 'GET'])
 def orders_list(carwash_id='', p=0):
     return list_orders(g, int(p), carwash_id)
