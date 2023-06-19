073518ea1f1c8e725255224de3a58eab84421837
Mr.Fill <mr.filldev@gmail.com>
2023-06-19 14:49:31 +0300

    upd reqs

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -345,7 +345,8 @@ def backend_search_prices(request, carwash_id):
 def backend_get_price_info(request, carwash_id):
     print('\n########################DATA####################################\n')
     data = request.form.to_dict()
-
+    price_id = request.args.get('price_id')
+    print('price_id: ', price_id)
     print(data, carwash_id)
     print('\n################################################################\n')
 
