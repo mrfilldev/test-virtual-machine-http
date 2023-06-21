f152675dc16dbb57a03fce34bf4a2f48c754b162
Mr.Fill <mr.filldev@gmail.com>
2023-06-21 15:18:54 +0300

    work on schedule

--- new_project/api/carwash_list.py
+++ new_project/api/carwash_list.py
@@ -24,7 +24,7 @@ def format_any_obj_id_to_Id(obj):
 def format_price_field(carwash_obj):
     print('carwash_obj.Price: ', carwash_obj.Price)
     if carwash_obj.Price is '(не указано)':
-        carwash_obj.Price = ''
+        carwash_obj.Price = []
     return carwash_obj
 
 
@@ -33,6 +33,7 @@ def format_everything(carwash_obj):
     print('carwash_obj changed _id to Id: \n', carwash_obj)
 
     carwash_obj = format_price_field(carwash_obj)
+    print('carwash_obj formatted .Price: \n', carwash_obj)
 
     return carwash_obj
 
