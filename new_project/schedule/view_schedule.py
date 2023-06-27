d78ad4ac3587d548d2b2718729f42e8a7fefbee5
Mr.Fill <mr.filldev@gmail.com>
2023-06-27 19:41:12 +0300

    work on schedule

--- new_project/schedule/view_schedule.py
+++ new_project/schedule/view_schedule.py
@@ -60,7 +60,7 @@ def get_orders(carwash_id):  # 7810324c8fea4af8bc3c3d6776cfc494
                 'start': order_obj.DateCreate,  # + timedelta(hours=3)
                 'date': order_obj.DateCreate,
                 'start_format': '' if order_obj.DateCreate == '' else parser.parse(
-                    order_obj.DateCreate).strftime('%H:%M'),
+                    convert_string_to_timezone(order_obj.DateCreate)).strftime('%H:%M'),
                 'resourceId': (chr(ord('`') + int(order_obj.BoxNumber))),
                 'box': order_obj.BoxNumber,
             })
