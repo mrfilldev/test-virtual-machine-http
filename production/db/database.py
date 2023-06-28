7dbeafb078c82bdc17ee6a37123bb2a108bbee8e
Mr.Fill <mr.filldev@gmail.com>
2023-06-28 19:00:12 +0300

    configs

--- production/db/database.py
+++ production/db/database.py
@@ -13,14 +13,14 @@ url = 'mongodb://{user}:{pw}@{hosts}/?replicaSet={rs}&authSource={auth_src}'.for
 client = pymongo.MongoClient(
     url,
     tlsCAFile='/home/mrfilldev/.mongodb/root.crt')
-db_test = client['production_db']
+db_production = client['production_db']
 
-col_orders = db_test["production_orders"]
-col_owners = db_test["production_owners"]
-col_carwashes_admins = db_test["production_carwashes_admins"]
-col_carwashes = db_test["production_carwashes"]
-col_prices = db_test["production_prices"]
-col_sets_of_prices = db_test["production_sets_of_prices"]
-col_companies = db_test["production_companies"]
-col_networks = db_test["production_networks"]
-col_users = db_test["production_users"]
+col_orders = db_production["production_orders"]
+col_owners = db_production["production_owners"]
+col_carwashes_admins = db_production["production_carwashes_admins"]
+col_carwashes = db_production["production_carwashes"]
+col_prices = db_production["production_prices"]
+col_sets_of_prices = db_production["production_sets_of_prices"]
+col_companies = db_production["production_companies"]
+col_networks = db_production["production_networks"]
+col_users = db_production["production_users"]
