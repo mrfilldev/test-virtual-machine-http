5347b1b7dc95b20094072a45cc50d944a8e6db4e
Mr.Fill <mr.filldev@gmail.com>
2023-06-28 17:42:00 +0300

    work on schedule

--- daemon2.0/start-service.sh
+++ daemon2.0/start-service.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export PYTHONPATH=..
-/home/mrfilldev/test-server/test-virtual-machine-http/venv/bin/python3 /home/mrfilldev/test-server/test-virtual-machine-http/daemon2.0/start_point2.0.py
+cd /home/mrfilldev/test-server/
+source venv/bin/activate
+cd /home/mrfilldev/test-server/test-virtual-machine-http/daemon2.0
+python3 start_point2.0.py
