8b7be281d86c6ad595b3fd646bc492914c73592b
Mr.Fill <mr.filldev@gmail.com>
2023-06-29 14:55:08 +0300

    configs

--- testing/start-service.sh
+++ testing/start-service.sh
@@ -5,10 +5,10 @@ cd /home/mrfilldev/test-server/test-virtual-machine-http/testing
 export FLASK_RUN_HOST=127.0.0.1
 
 # DEV = 9999
-#export FLASK_RUN_PORT=9999
+export FLASK_RUN_PORT=9999
 
 # PROD = 6666
-export FLASK_RUN_PORT=6666
+#export FLASK_RUN_PORT=6666
 
 #export FLASK_RUN_PORT=8080
 
