6869757393e05537c0dfa99999bbefdb2caaf08d
Mr.Fill <mr.filldev@gmail.com>
2023-06-29 16:40:25 +0300

    configs

--- testing/db/database.py
+++ testing/db/database.py
@@ -9,13 +9,13 @@ tlsCAFile = Config.tlsCAFile
 client = pymongo.MongoClient(url, tlsCAFile=tlsCAFile)
 db_production = client['test_database']
 
-col_orders = db_production["testing_orders"]
-col_owners = db_production["testing_owners"]
-col_carwashes_admins = db_production["testing_carwashes_admins"]
-col_carwashes = db_production["testing_carwashes"]
-col_prices = db_production["testing_prices"]
+col_orders = db_production["test_orders"]
+col_owners = db_production["test_owners"]
+col_carwashes_admins = db_production["test_carwashes_admins"]
+col_carwashes = db_production["test_carwashes"]
+col_prices = db_production["test_prices"]
 col_sets_of_prices = db_production["sets_of_prices"]
-col_companies = db_production["testing_companies"]
-col_networks = db_production["testing_networks"]
-col_users = db_production["testing_users"]
+col_companies = db_production["test_companies"]
+col_networks = db_production["test_networks"]
+col_users = db_production["test_users"]
 
