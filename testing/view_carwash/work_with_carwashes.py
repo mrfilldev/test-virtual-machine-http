ea54f3f48d314464b0324c1fc3954d0cf4807004
Mr.Fill <mr.filldev@gmail.com>
2023-07-06 20:51:41 +0300

    available sets by network id

--- testing/view_carwash/work_with_carwashes.py
+++ testing/view_carwash/work_with_carwashes.py
@@ -147,7 +147,6 @@ def list_carwashes(g):
         data = json.dumps(data, default=lambda x: x.__dict__)
         network_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))[0]  # SimpleNamespace
         print('network_obj:', network_obj)
-
     else:
         all_carwashes = database.col_carwashes.find({})
         network_obj = None
