22d196970e8a528c507152d2b751a518910b0de2
Mr.Fill <mr.filldev@gmail.com>
2023-07-06 19:38:58 +0300

    available sets by network id

--- testing/statistics/backend.py
+++ testing/statistics/backend.py
@@ -2,7 +2,7 @@ from flask import render_template
 
 
 def get_statistics(g_user_flask):
-    role = g_user_flask.db['role']
+    role = g_user_flask.user_db['role']
 
     context = {
         'role': role,
