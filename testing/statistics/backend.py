2c5519a4a0789dddd1a326f69505ce8a4191aa57
Mr.Fill <mr.filldev@gmail.com>
2023-07-10 19:28:01 +0300

    beauty

--- testing/statistics/backend.py
+++ testing/statistics/backend.py
@@ -24,17 +24,13 @@ colors = [
 
 def try_shit():
     message = "################################ \n"
-
-    start_time = str(datetime.now())
-
-    print('start_time', type(start_time), start_time)
-    pipeline = [
-        # {"$match": {"DateCreateMy": {"$gte": start_time}}},
-        {"$group": {"_id": "$Status",
-                    # "CarWashId": "$CarWashId",
-                    "total": {"$sum": "$Sum"},
-                    "count": {"$sum": 1}}}
-    ]
+    pipeline_by_status = [{
+        "$group": {
+            "_id": "$Status",
+            "total": {"$sum": "$Sum"},
+            "count": {"$sum": 1}
+        }
+    }]
     result = database.col_orders.aggregate(pipeline)
     # Выводим результаты
     message += "Сводка статусов заказов за все время:\n"
@@ -51,9 +47,32 @@ def try_shit():
     print(message)
 
 
+def try_group_date():
+    message = "################################ \n"
+    pipline_date = [{
+        "$group": {
+            "_id": {"$dayOfYear": "$date"},
+            "amount": {"$sum": 1}}}
+    ]
+    result = database.col_orders.aggregate(pipline_date)
+    # Выводим результаты
+    amount_of_orders = 0
+    for doc in result:
+        print(doc)
+        # message += f"{doc['CarWashId']}:\n"
+        message += f"""\n{doc['_id']} -> {doc['count']} шт. = {doc['total']} руб.\n"""
+        message += '\n'
+        amount_of_orders += int(doc['count'])
+    message += str(amount_of_orders)
+    message += '\n'
+    message += "################################"
+    print(message)
+
+
 def get_statistics(g_user_flask):
     try_shit()
-
+    print('групировка по дате: \n')
+    try_group_date()
     context = {
         'max': 20,
         'labels': labels,
@@ -68,45 +87,43 @@ def get_statistics(g_user_flask):
         context=context
     )
 
-
-
 # print("################################")
-    # start_time = str(datetime.now())
-    # print(start_time)
-    # message += "\n За последние 15 минут: \n"
-    # now = datetime.now()
-    # interval = now - timedelta(minutes=15)
-    # print(interval)
-    # агрегация заказов за последние 15 минут
-    # start_time = datetime.utcnow() - timedelta(minutes=15)
-    # print(start_time)
-    # message += '\n'
-    # message += "################################"
-    #
-    # print("################################")
-    # print(message)
-    # выполнить агрегацию
-    # query = {
-    #     'DateCreate': {'$gt': start_time.isoformat()}
-    # }
-    # pipeline = [
-    #     {
-    #         '$match': {
-    #             'DateCreate': {'$gt': start_time.isoformat()}
-    #         }
-    #     },
-    #     {
-    #         '$group': {
-    #             '_id': '$Status',
-    #             'count': {'$sum': 1},
-    #             "total": {"$sum": "$Sum"},
-    #         }
-    #     }
-    # ]
-    # message += '\n ПОЛУЧАЕМЫЙ ОБЪЕКТ АГГРЕГАЦИИ: \n'
-    # amount_of_orders = 0
-    # for doc in result:
-    #     print(doc)
-    #     message += str(doc)
-    #     message += f"""\n{doc['_id']} -> {doc['count']} шт. = {doc['total']} руб.\n"""
-    #     amount_of_orders += doc['count']
+# start_time = str(datetime.now())
+# print(start_time)
+# message += "\n За последние 15 минут: \n"
+# now = datetime.now()
+# interval = now - timedelta(minutes=15)
+# print(interval)
+# агрегация заказов за последние 15 минут
+# start_time = datetime.utcnow() - timedelta(minutes=15)
+# print(start_time)
+# message += '\n'
+# message += "################################"
+#
+# print("################################")
+# print(message)
+# выполнить агрегацию
+# query = {
+#     'DateCreate': {'$gt': start_time.isoformat()}
+# }
+# pipeline = [
+#     {
+#         '$match': {
+#             'DateCreate': {'$gt': start_time.isoformat()}
+#         }
+#     },
+#     {
+#         '$group': {
+#             '_id': '$Status',
+#             'count': {'$sum': 1},
+#             "total": {"$sum": "$Sum"},
+#         }
+#     }
+# ]
+# message += '\n ПОЛУЧАЕМЫЙ ОБЪЕКТ АГГРЕГАЦИИ: \n'
+# amount_of_orders = 0
+# for doc in result:
+#     print(doc)
+#     message += str(doc)
+#     message += f"""\n{doc['_id']} -> {doc['count']} шт. = {doc['total']} руб.\n"""
+#     amount_of_orders += doc['count']
