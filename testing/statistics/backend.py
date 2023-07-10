69c95b846e37f0ca6a4f350c54dca91895a4faab
Mr.Fill <mr.filldev@gmail.com>
2023-07-10 19:32:21 +0300

    beauty

--- testing/statistics/backend.py
+++ testing/statistics/backend.py
@@ -51,7 +51,7 @@ def try_group_date():
     message = "################################ \n"
     pipline_date = [{
         "$group": {
-            "_id": {"$dayOfYear": "$DateCreate"},
+            "_id": {"$dayOfYear": "DateCreate"},
             "amount": {"$sum": 1}}}
     ]
     result = database.col_orders.aggregate(pipline_date)
