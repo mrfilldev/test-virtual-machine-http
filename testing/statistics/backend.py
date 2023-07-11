1963386bcd42f34e5b3e5233465d1c575f813f16
Mr.Fill <mr.filldev@gmail.com>
2023-07-11 13:43:32 +0300

    beauty

--- testing/statistics/backend.py
+++ testing/statistics/backend.py
@@ -100,9 +100,9 @@ def group_by_date():
 
 
 def get_statistics(g_user_flask):
-    research_by_status = group_by_date()
+    research_by_date = group_by_date()
     print('групировка по дате: \n')
-    research_by_date = group_by_status()
+    research_by_status = group_by_status()
     context = {
         'max': 20,
         'labels': labels,
