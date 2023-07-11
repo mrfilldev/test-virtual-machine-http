15e0993211a392af55850c29cbd8d3c61e668441
Mr.Fill <mr.filldev@gmail.com>
2023-07-11 15:23:31 +0300

    beauty

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -189,3 +189,19 @@ def fix_date_users():
             "date_registered": parse(user_obj.date_registered).isoformat(),
         }})
     return redirect(url_for('admin_blueprint.admin_main'))
+
+
+def fix_date_orders():
+    all_orders = database.col_orders.find({})
+
+    for order in all_orders:
+        data = json.loads(json_util.dumps(order))
+        data = json.dumps(data, default=lambda x: x.__dict__)
+        order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        try:
+            print(order_obj.DateCreate, parse(order_obj.DateCreate), type(parse(order_obj.DateCreate)), parse(order_obj.DateCreate).isoformat())
+            print(order_obj.DateStart, parse(order_obj.DateStart), type(parse(order_obj.DateStart)), parse(order_obj.DateStart).isoformat())
+            print(order_obj.DateEnd, parse(order_obj.DateEnd), type(parse(order_obj.DateEnd)), parse(order_obj.DateEnd).isoformat())
+        except Exception as e:
+            pass
+    return redirect(url_for('admin_blueprint.admin_main'))
