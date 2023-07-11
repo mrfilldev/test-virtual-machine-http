c013047699ae61ee2c9314803b87d673f2bb6e12
Mr.Fill <mr.filldev@gmail.com>
2023-07-11 16:33:17 +0300

    beauty

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -205,9 +205,9 @@ def fix_date_orders():
             if order_obj.DateCreate[-12:] == "+00:00+00:00":
 
                 print('WORK', order_obj.DateCreate.removesuffix("+00:00"))
-                database.col_orders.update_one({'_id': order_obj._id}, {"$set": {
-                    "DateCreate": order_obj.DateCreate.removesuffix("+00:00"),
-                }})
+                # database.col_orders.update_one({'_id': order_obj._id}, {"$set": {
+                #     "DateCreate": order_obj.DateCreate.removesuffix("+00:00"),
+                # }})
             # else:
             #     database.col_orders.update_one({'_id': order_obj._id}, {"$set": {
             #         "DateCreate": order_obj.DateCreate + '+00:00',
