14d05cb481b7c87c47038bce5152f2acd4cc5ac5
Mr.Fill <mr.filldev@gmail.com>
2023-07-11 16:11:39 +0300

    beauty

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -200,7 +200,7 @@ def fix_date_orders():
         data = json.dumps(data, default=lambda x: x.__dict__)
         order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
         try:
-            print(order_obj.DateCreate, type(order_obj.DateCreate))
+            print(order_obj.DateCreate, type(order_obj.DateCreate), parse(order_obj.DateCreate))
             # local = pytz.timezone("Europe/Moscow")
             # local_dt = local.localize(order_obj.DateCreate, is_dst=None)
             # utc_dt = local_dt.astimezone(pytz.utc)
