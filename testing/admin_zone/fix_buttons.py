7b488d1d3f5c6760cbaea5b0e5bc7b6e52624809
Mr.Fill <mr.filldev@gmail.com>
2023-07-11 15:32:15 +0300

    beauty

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -199,11 +199,11 @@ def fix_date_orders():
         data = json.dumps(data, default=lambda x: x.__dict__)
         order_obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
         try:
-            print(order_obj.DateCreate, parse(order_obj.DateCreate), type(parse(order_obj.DateCreate)), parse(order_obj.DateCreate).isoformat())
-            # print(order_obj.DateStart, parse(order_obj.DateStart), type(parse(order_obj.DateStart)), parse(order_obj.DateStart).isoformat())
+            # print(order_obj.DateCreate, parse(order_obj.DateCreate), type(parse(order_obj.DateCreate)), parse(order_obj.DateCreate).isoformat())
+            print(order_obj.DateStart, parse(order_obj.DateStart), type(parse(order_obj.DateStart)), parse(order_obj.DateStart).isoformat())
             # print(order_obj.DateEnd, parse(order_obj.DateEnd), type(parse(order_obj.DateEnd)), parse(order_obj.DateEnd).isoformat())
             database.col_orders.update_one({'_id': order_obj._id}, {"$set": {
-                "DateCreate": parse(order_obj.DateCreate).isoformat(),
+                "DateStart": parse(order_obj.DateStart).isoformat(),
             }})
         except Exception as e:
             pass
