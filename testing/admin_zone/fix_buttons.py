c11fa3f12c25d6ba20a6cbfc3fdae83a685fee4d
Mr.Fill <mr.filldev@gmail.com>
2023-07-12 19:16:03 +0300

    beauty

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -205,22 +205,22 @@ def default(obj):
 def fix_date_orders():
     all_orders = database.col_orders.find({})
 
-    order = all_orders[0]
-    try:
-        # print('\norder: ', order)
-        # # Serialize ``obj`` to a JSON formatted ``str``.
-        # test_obj = json.dumps(order, default=default)
-        # print('\ntest_obj: ', test_obj)
-        # # Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance containing a JSON document) to a Python
-        # # object.
-        # order_obj = json.loads(test_obj, object_hook=lambda d: SimpleNamespace(**d))
-        # print('\norder_obj: ', order_obj)
-        # print('\n')
-        print('order: ', order)
-        order_obj = SimpleNamespace(**order)
-        print('order_obj: ', order_obj)
-    except Exception as e:
-        print(e)
+    for order in all_orders:
+        try:
+            # print('\norder: ', order)
+            # # Serialize ``obj`` to a JSON formatted ``str``.
+            # test_obj = json.dumps(order, default=default)
+            # print('\ntest_obj: ', test_obj)
+            # # Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance containing a JSON document) to a Python
+            # # object.
+            # order_obj = json.loads(test_obj, object_hook=lambda d: SimpleNamespace(**d))
+            # print('\norder_obj: ', order_obj)
+            # print('\n')
+            print('order: ', order)
+            order_obj = SimpleNamespace(**order)
+            print('order_obj: ', order_obj)
+        except Exception as e:
+            print(e)
 
     return redirect(url_for('admin_blueprint.admin_main'))
 
