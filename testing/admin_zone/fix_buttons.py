8ec8846548d0d95ca9c3b299d32ee927c4b44069
Mr.Fill <mr.filldev@gmail.com>
2023-07-14 16:12:45 +0300

    fixing database date

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -181,17 +181,23 @@ def fix_sets():
 
 def fix_date_users():
     all_users = database.col_users.find({})
-
     for user in all_users:
-        print('\nuser: ', user)
-        # Serialize ``obj`` to a JSON formatted ``str``.
-        test_obj = json.dumps(user, default=default)
-        print('\ntest_obj: ', test_obj)
-        # Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance containing a JSON document) to a Python
-        # object.
-        user_obj = json.loads(test_obj, object_hook=lambda d: SimpleNamespace(**d))
-        print('\nuser_obj: ', user_obj)
-        print('\n')
+        try:
+            print('\nuser: ', user)
+            # Serialize ``obj`` to a JSON formatted ``str``.
+            test_obj = json.dumps(user, default=default)
+            print('\ntest_obj: ', test_obj)
+            # Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance containing a JSON document) to a Python
+            # object.
+            user_obj = json.loads(test_obj, object_hook=lambda d: SimpleNamespace(**d))
+            print('\nuser_obj: ', user_obj)
+            print('\n')
+
+            if "+00:00" in user_obj.date_registered:
+                user_obj.removesuffix("+00:00")
+        except Exception as e:
+            print(e)
+
         #
         # database.col_users.update_one({'_id': user_obj._id}, {"$set": {
         #     "date_registered": parse(user_obj.date_registered).isoformat(),
