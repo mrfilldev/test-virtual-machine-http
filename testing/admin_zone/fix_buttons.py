83d0f8f25bb9f3483ce790af5186357a92a00ba5
Mr.Fill <mr.filldev@gmail.com>
2023-07-14 16:18:53 +0300

    fixing database date

--- testing/admin_zone/fix_buttons.py
+++ testing/admin_zone/fix_buttons.py
@@ -184,7 +184,7 @@ def fix_date_users():
     all_users = database.col_users.find({})
     for user in all_users:
         try:
-            # print('\nuser: ', user)
+            print('\nuser: ', user)
             test_obj = json.dumps(user, default=default)
             # print('\ntest_obj: ', test_obj)
             user_obj = json.loads(test_obj, object_hook=lambda d: SimpleNamespace(**d))
