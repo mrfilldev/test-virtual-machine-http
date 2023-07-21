e02e2176db3b53e04d092e96eb09f6fd89e0dbf3
Mr.Fill <mr.filldev@gmail.com>
2023-07-21 17:49:06 +0300

    statistics

--- production/configuration/config.py
+++ production/configuration/config.py
@@ -22,25 +22,6 @@ class Config(object):
     YAN_CLIENT_SECRET = os.environ.get('PRODUCTION_YAN_CLIENT_SECRET')
 
 
-class Config(object):
-    PYMONGO_URL = os.environ.get('PYMONGO_PRODUCTION_URL')
-    tlsCAFile = os.environ.get('tlsCAFile')
-
-    URL_DEV = os.environ.get('URL_DEV')
-    API_KEY = os.environ.get('API_KEY')
-
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-
-    CHANNEL_ID = os.environ.get('CHANNEL_ID')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-
-    # yandex oauth
-    YAN_CLIENT_ID = os.environ.get('TESTING_YAN_CLIENT_ID')
-    YAN_CLIENT_SECRET = os.environ.get('TESTING_YAN_CLIENT_SECRET')
-
-
 class Sqs_params:
     client = boto3.client(
         aws_access_key_id=Config.AWS_ACCESS_KEY_ID,  # os.getenv('AWS_ACCESS_KEY_ID'),
