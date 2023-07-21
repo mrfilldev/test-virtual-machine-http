b5f2ba98c6bead196850f00b22a8ce206efc333a
Mr.Fill <mr.filldev@gmail.com>
2023-07-21 17:45:42 +0300

    statistics

--- production/start-service.sh
+++ production/start-service.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 cd /home/mrfilldev/test-server/
 source venv/bin/activate
-cd /home/mrfilldev/test-server/test-virtual-machine-http/testing
+cd /home/mrfilldev/test-server/test-virtual-machine-http/production
 export FLASK_RUN_HOST=127.0.0.1
 
 # DEV = 9999
 export FLASK_RUN_PORT=6666
-
-# PROD = 6666
-#export FLASK_RUN_PORT=6666
 
 #export FLASK_RUN_PORT=8080
 
