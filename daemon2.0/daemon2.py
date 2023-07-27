4c196ced1eec0fa78a01960a253974207cd8ffa9
Mr.Fill <mr.filldev@gmail.com>
2023-07-27 15:00:06 +0300

    statistics

--- daemon2.0/daemon2.py
+++ daemon2.0/daemon2.py
@@ -131,6 +131,7 @@ async def update_order_status(order, status):
         "Status": status,
         "DateEnd": parse(order['DateEnd'])
     }}
+    print(set_command)
 
     upd_order = Py_mongo_db.col_orders.update_one(old_order, set_command)
     print('updated order: ', upd_order)
